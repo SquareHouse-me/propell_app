93e7e1f2176ae093611fb05b033d62285f0b3de2
sh-flutter-01 <153494492+sh-flutter-01@users.noreply.github.com>
2025-01-18 10:00:59 +0500

    update the home spacing

--- lib/views/dashBoard/home_view.dart
+++ lib/views/dashBoard/home_view.dart
@@ -42,102 +42,110 @@ class HomeView extends StatelessWidget {
                       ),
                     )
                   : SingleChildScrollView(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 16.w),
-                        child: ResponsiveRowColumn(
-                          layout: ResponsiveRowColumnType.COLUMN,
-                          columnCrossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ResponsiveRowColumnItem(child: 40.heightSpace),
-                            // isMobile
-                            //     ?
-                            ResponsiveRowColumnItem(
-                              child: Padding(
-                                padding: EdgeInsets.only(
-                                    left: isMobile ? 0.0 : 25.0),
-                                child: TextComponents(
-                                  color: Colors.white,
-                                  title: 'Book an appointment',
-                                  size: isMobile ? 20.sp : 20,
-                                  weight: FontWeight.w500,
+                      child: Center(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 16.w),
+                          child: ResponsiveRowColumn(
+                            layout: ResponsiveRowColumnType.COLUMN,
+                            columnCrossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ResponsiveRowColumnItem(child: 40.heightSpace),
+                              // isMobile
+                              //     ?
+                              ResponsiveRowColumnItem(
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                      left: isMobile ? 0.0 : 25.0),
+                                  child: TextComponents(
+                                    color: Colors.white,
+                                    title: 'Book an appointment',
+                                    size: isMobile ? 20.sp : 20,
+                                    weight: FontWeight.w500,
+                                  ),
                                 ),
                               ),
-                            ),
-                            // : const ResponsiveRowColumnItem(
-                            //     child: WebHeaderTitle()),
-                            ResponsiveRowColumnItem(
-                              child: 20.heightSpace,
-                            ),
-                            ResponsiveRowColumnItem(
-                                child: SizedBox(
-                              width: isMobile ? null : 1100,
-                              child: ResponsiveGridList(
-                                shrinkWrap: true,
+                              // : const ResponsiveRowColumnItem(
+                              //     child: WebHeaderTitle()),
+                              ResponsiveRowColumnItem(
+                                child: 20.heightSpace,
+                              ),
+                              ResponsiveRowColumnItem(
+                                  child: SizedBox(
+                                width: isMobile ? null : 1100,
+                                child: ResponsiveGridList(
+                                  shrinkWrap: true,
 
-                                horizontalGridSpacing: isMobile
-                                    ? 2.sp
-                                    : 16, // Horizontal space between grid items
-                                verticalGridSpacing: isMobile
-                                    ? 16.sp
-                                    : 14, // Vertical space between grid items
-                                horizontalGridMargin: isMobile
-                                    ? 0.sp
-                                    : 17, // Horizontal space around the grid
-                                verticalGridMargin: isMobile
-                                    ? 0.sp
-                                    : 15, // Vertical space around the grid
-                                minItemWidth:
-                                    500, // The minimum item width (can be smaller, if the layout constraints are smaller)
-                                minItemsPerRow: isMobile
-                                    ? 1
-                                    : 3, // The minimum items to show in a single row. Takes precedence over minItemWidth
-                                maxItemsPerRow:
-                                    3, // The maximum items to show in a single row. Can be useful on large screens
-                                listViewBuilderOptions: ListViewBuilderOptions(
-                                    physics:
-                                        const NeverScrollableScrollPhysics(),
-                                    padding: EdgeInsets
-                                        .zero), // Options that are getting passed to the ListView.builder() function
-                                children: homeC.categoryList.value.map((item) {
-                                  return buildOptionCard(
-                                    id: item.id,
-                                    context,
-                                    icon: item
-                                        .image, // Use the appropriate field for the icon
-                                    label: item.name.split(' ')[
-                                        0], // Use the appropriate field for the label
-                                    subTitle: item.name.split(' ')[
-                                        1], // Use the appropriate field for the subtitle
-                                  );
-                                }).toList(), // The list of widgets in the list
-                              ),
-                            )),
-                            ResponsiveRowColumnItem(child: 12.heightSpace),
-                            ResponsiveRowColumnItem(
+                                  horizontalGridSpacing: isMobile
+                                      ? 2.sp
+                                      : 16, // Horizontal space between grid items
+                                  verticalGridSpacing: isMobile
+                                      ? 16.sp
+                                      : 14, // Vertical space between grid items
+                                  horizontalGridMargin: isMobile
+                                      ? 0.sp
+                                      : 17, // Horizontal space around the grid
+                                  verticalGridMargin: isMobile
+                                      ? 0.sp
+                                      : 15, // Vertical space around the grid
+                                  minItemWidth:
+                                      500, // The minimum item width (can be smaller, if the layout constraints are smaller)
+                                  minItemsPerRow: isMobile
+                                      ? 1
+                                      : 3, // The minimum items to show in a single row. Takes precedence over minItemWidth
+                                  maxItemsPerRow:
+                                      3, // The maximum items to show in a single row. Can be useful on large screens
+                                  listViewBuilderOptions: ListViewBuilderOptions(
+                                      physics:
+                                          const NeverScrollableScrollPhysics(),
+                                      padding: EdgeInsets
+                                          .zero), // Options that are getting passed to the ListView.builder() function
+                                  children:
+                                      homeC.categoryList.value.map((item) {
+                                    return buildOptionCard(
+                                      id: item.id,
+                                      context,
+                                      icon: item
+                                          .image, // Use the appropriate field for the icon
+                                      label: item.name.split(' ')[
+                                          0], // Use the appropriate field for the label
+                                      subTitle: item.name.split(' ')[
+                                          1], // Use the appropriate field for the subtitle
+                                    );
+                                  }).toList(), // The list of widgets in the list
+                                ),
+                              )),
+                              ResponsiveRowColumnItem(child: 12.heightSpace),
+                              ResponsiveRowColumnItem(
+                                  child: Padding(
+                                padding: EdgeInsets.only(
+                                    left: isMobile ? 0.0 : 25.0),
                                 child: SizedBox(
-                              width: isMobile ? null : 1100,
-                              child: Image.network(
-                                homeC.sliderList.value.first.image.toString(),
-                                fit: BoxFit.fill,
-                                loadingBuilder: (BuildContext context,
-                                    Widget child,
-                                    ImageChunkEvent? loadingProgress) {
-                                  if (loadingProgress == null)
-                                    return child; // Return the image once loaded
-                                  return getIndicator(); // Show the custom loading indicator
-                                },
-                                errorBuilder: (BuildContext context,
-                                    Object error, StackTrace? stackTrace) {
-                                  return Icon(
-                                    Icons.error,
-                                    color: AppColor
-                                        .kGreen1Color, // Custom error icon and color
-                                  );
-                                },
-                              ),
-                            )),
-                            ResponsiveRowColumnItem(child: 36.heightSpace),
-                          ],
+                                  width: isMobile ? null : 1100,
+                                  child: Image.network(
+                                    homeC.sliderList.value.first.image
+                                        .toString(),
+                                    fit: BoxFit.fill,
+                                    loadingBuilder: (BuildContext context,
+                                        Widget child,
+                                        ImageChunkEvent? loadingProgress) {
+                                      if (loadingProgress == null)
+                                        return child; // Return the image once loaded
+                                      return getIndicator(); // Show the custom loading indicator
+                                    },
+                                    errorBuilder: (BuildContext context,
+                                        Object error, StackTrace? stackTrace) {
+                                      return Icon(
+                                        Icons.error,
+                                        color: AppColor
+                                            .kGreen1Color, // Custom error icon and color
+                                      );
+                                    },
+                                  ),
+                                ),
+                              )),
+                              ResponsiveRowColumnItem(child: 36.heightSpace),
+                            ],
+                          ),
                         ),
                       ),
                     )
